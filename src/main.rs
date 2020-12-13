f2367b2cd88acd1b2a67713e665db6d9e7750110
Evie Viau <evie.viau@gmail.com>
2020-12-13 14:18:53 -0500

    Optimize database writes and improve if statements

--- src/main.rs
+++ src/main.rs
@@ -41,16 +41,6 @@ impl EventHandler for Handler {
                 .await
                 .unwrap();
 
-            if let Err(why) = msg.channel_id.edit(&ctx.http, |c| c.topic(format!("Next number: {} | Last Submission User: <@{}> | Highest Count: {} | Times Failed: {} | Last Failed Submission User: <@{}>",
-                    1,
-                    "",
-                    0,
-                    0,
-                    ""
-                ))).await {
-                println!("Error editing channel topic: {:?}", why);
-            } 
-
             if let Err(why) = msg.channel_id.say(&ctx.http, "Setting current channel as counting channel!").await {
                 println!("Error sending message: {:?}", why);
             }
@@ -80,17 +70,9 @@ impl EventHandler for Handler {
                 println!("Error sending message: {:?}", why);
             }
 
-            if let Err(why) = msg.channel_id.edit(&ctx.http, |c| c.topic(format!("Next number: {} | Last Submission User: <@{}> | Highest Count: {} | Times Failed: {} | Last Failed Submission User: <@{}>",
-                    1,
-                    "",
-                    0,
-                    0,
-                     ""
-                ))).await {
-                 println!("Error editing channel topic: {:?}", why);
-             } 
         } else if msg_arguments[0].parse::<i32>().is_ok() {
             let submission = msg_arguments[0].parse::<i32>().unwrap();
+            let mut submission_passed = true;
 
             let results = servers.filter(guild_id.eq(org_guild_id.clone()))
                 .load_async::<Servers>(db)
@@ -125,116 +107,91 @@ impl EventHandler for Handler {
                 return;
             }
 
-            if submission != guild_info.current_count + 1 || msg.author.id.0.to_string() == guild_info.last_submission_user {
-                if guild_info.gamemode == 1 || guild_info.gamemode == 2 {
-                    guild_info.times_failed += 1;
-
-                    if let Err(why) = msg.channel_id.say(&ctx.http, format!("<@{}> has broken the chain! Next number was {}!", msg.author.id.0, guild_info.current_count + 1)).await {
-                        println!("Error sending message: {:?}", why);
-                    }    
-                    diesel::update(servers.filter(guild_id.eq(org_guild_id.clone())))
-                        .set((
-                            times_failed.eq(guild_info.times_failed),
-                            last_failed_user.eq(msg.author.id.0.to_string()),
-                            current_count.eq(0),
-                            last_submission_user.eq("".to_string())
-                        ))
-                        .execute_async(db)
-                        .await
-                        .expect("Error updating the database");
-                    
-                        if let Err(why) = msg.channel_id.edit(&ctx.http, |c| c.topic(format!("Next number: {} | Last Submission User: <@{}> | Highest Count: {} | Times Failed: {} | Last Failed Submission User: <@{}>",
-                                1,
-                                "",
-                                &guild_info.highest_count,
-                                &guild_info.times_failed,
-                                msg.author.id.0
-                            ))).await {
-                            println!("Error editing channel topic: {:?}", why);
-                        } 
-
-                    return;
-                } else if guild_info.gamemode == 0 {
-                    return;
-                }
-            }
-
             if submission > guild_info.highest_count {
                 guild_info.highest_count = submission.clone();
-
-                diesel::update(servers.filter(guild_id.eq(org_guild_id.clone())))
-                    .set(highest_count.eq(guild_info.highest_count))
-                    .execute_async(db)
-                    .await
-                    .expect("Error updating the database");
             }
 
+            let mut same_auth_in_two = false;
             if guild_info.gamemode == 2 {
                 guild_info.last_submission_user = msg.author.id.0.to_string();
 
-                diesel::update(servers.filter(guild_id.eq(org_guild_id.clone())))
-                .set(last_submission_user.eq(guild_info.last_submission_user.clone()))
-                .execute_async(db)
-                .await
-                .expect("Error updating the database");
+                if msg.author.id.0.to_string() == guild_info.last_submission_user {
+                    same_auth_in_two = true;
+                }
             }
 
-            guild_info.current_count = submission.clone();
-            
+            if submission != guild_info.current_count + 1 || same_auth_in_two {
+                if guild_info.gamemode != 0 {
+                    if let Err(why) = msg.channel_id.say(&ctx.http, format!("<@{}> has broken the chain! Next number was {}!", msg.author.id.0, guild_info.current_count + 1)).await {
+                        println!("Error sending message: {:?}", why);
+                    }
+
+                    guild_info.times_failed += 1;
+                    guild_info.last_failed_user = msg.author.id.0.to_string();
+                    guild_info.current_count = 0;
+                    guild_info.last_submission_user = "".to_string();
+
+                    submission_passed = false;
+                }
+            } else {
+                guild_info.current_count = submission.clone();
+            }
+
+            if submission_passed {
+                if let Err(why) = msg.react(&ctx.http, serenity::model::channel::ReactionType::Unicode("✅".to_string())).await {
+                    println!("Error reacting to message: {:?}", why);
+                }
+            }
+
             diesel::update(servers.filter(guild_id.eq(org_guild_id.clone())))
-                .set(current_count.eq(guild_info.current_count))
+                .set((
+                        guild_id.eq(guild_info.guild_id),
+                        channel_id.eq(guild_info.channel_id),
+                        current_count.eq(guild_info.current_count),
+                        last_submission_user.eq(guild_info.last_submission_user),
+                        highest_count.eq(guild_info.highest_count),
+                        times_failed.eq(guild_info.times_failed),
+                        last_failed_user.eq(guild_info.last_failed_user),
+                        gamemode.eq(guild_info.gamemode)
+                    ))
                 .execute_async(db)
                 .await
                 .expect("Error updating the database");
-
-            if let Err(why) = msg.react(&ctx.http, serenity::model::channel::ReactionType::Unicode("✅".to_string())).await {
-                println!("Error reacting to message: {:?}", why);
-            } 
-
-            if let Err(why) = msg.channel_id.edit(&ctx.http, |c| c.topic(format!("Next number: {} | Last Submission User: <@{}> | Highest Count: {} | Times Failed: {} | Last Failed Submission User: <@{}>",
-                    &guild_info.current_count,
-                    guild_info.last_submission_user.clone(),
-                    &guild_info.highest_count,
-                    &guild_info.times_failed,
-                    guild_info.last_failed_user.clone()
-                ))).await {
-                println!("Error editing channel topic: {:?}", why);
-            } 
-        }
     }
-    
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
+}
+
+async fn ready(&self, _: Context, ready: Ready) {
+    println!("{} is connected!", ready.user.name);
+}
 }
 
 
 struct DbConn;
 
 impl TypeMapKey for DbConn {
-    type Value = Pool<ConnectionManager<SqliteConnection>>;
+type Value = Pool<ConnectionManager<SqliteConnection>>;
 }
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
+dotenv::dotenv().ok();
 
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+let token = env::var("DISCORD_TOKEN")
+    .expect("Expected a token in the environment");
 
-    let db_url = env::var("DATABASE_URL")
-        .expect("Expected DATABASE_URL to be populated");
-    
-    let mut client = Client::builder(&token)
-        .event_handler(Handler)
-        .await
-        .expect("Err creating client");
-        {
-            let mut data = client.data.write().await;
-            data.insert::<DbConn>(Pool::builder().build(ConnectionManager::<SqliteConnection>::new(db_url)).unwrap());
-        }
-    
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+let db_url = env::var("DATABASE_URL")
+    .expect("Expected DATABASE_URL to be populated");
+
+let mut client = Client::builder(&token)
+    .event_handler(Handler)
+    .await
+    .expect("Err creating client");
+    {
+        let mut data = client.data.write().await;
+        data.insert::<DbConn>(Pool::builder().build(ConnectionManager::<SqliteConnection>::new(db_url)).unwrap());
     }
+
+if let Err(why) = client.start().await {
+    println!("Client error: {:?}", why);
+}
 }
